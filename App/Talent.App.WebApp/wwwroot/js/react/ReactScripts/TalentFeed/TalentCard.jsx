ea039675517138dcd9613cbf205047155d733142
Kiran <parmarkiran245@gmail.com>
2021-08-27 14:26:23 +1200

    update
    update

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentCard.jsx
@@ -0,0 +1,120 @@
+﻿import React from 'react';
+import ReactPlayer from 'react-player';
+import PropTypes from 'prop-types'
+import { Button, Icon ,Card,Image, Container,Item,Grid, Header} from 'semantic-ui-react'
+
+export default class TalentCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state=
+        {
+            id:'',
+            display:'Video',
+            EmployerDetail:[],
+        }
+       this.onClickVideo=this.onClickVideo.bind(this)
+       this.onClickProfile=this.onClickProfile.bind(this)
+    };
+    onClickVideo()
+    {
+        this.setState(
+            {
+                display:null,
+                id:null
+            }
+        )
+    }
+    onClickProfile(event)
+    {
+        console.log("Profile" + event.target.id )
+        this.setState(
+            {
+                display:"Profile",
+                id:event.target.id
+            }
+        )
+    }
+    render()
+    {
+        const src='https://semantic-ui.com/images/avatar2/large/kristy.png'
+        console.log("video")
+        let disply;
+        return(
+            <div>
+                {this.props.talentDetail.map((talent) => (
+                <Card fluid key={talent.id}>
+                <Card.Content>
+                <Card.Header><Container textAlign='right'><Icon name='star'/></Container>{talent.name}</Card.Header>
+                </Card.Content>
+                <Card.Content>
+                <Card.Content> 
+                   {
+                       this.state.display == "Profile" && talent.id == this.state.id
+                       ?
+                       <Item>
+                        <Image src={src} size='small' floated='left' />
+                            <Item.Content>
+                                <Item.Description>
+                                    <Header sub> Heading {talent.name}</Header>
+                                    <Header as='h5' sub>
+                                    Current Employer
+                                        <Header.Subheader>
+                                        {talent.currentEmployment}
+                                        </Header.Subheader>
+                                    </Header>
+                                    <Header as='h5' sub>
+                                    Visa Status
+                                        <Header.Subheader>
+                                        {talent.visa}
+                                        </Header.Subheader>
+                                    </Header>
+                                    <Header as='h5' sub>
+                                    Position
+                                        <Header.Subheader>
+                                        {talent.level}
+                                        </Header.Subheader>
+                                    </Header>
+                                </Item.Description>
+                            </Item.Content>
+                        </Item>
+                       :
+                       <ReactPlayer  width='520px'
+                        url={talent.videoUrl ? talent.videoUrl : 'https://www.youtube.com/watch?v=ysz5S6PUM-U'}
+                        />
+                   }
+                </Card.Content>
+                </Card.Content>
+                <Card.Content extra>
+                    <Header>
+                    <Grid>
+                    <Grid.Row columns={4}>
+                        <Grid.Column>
+                        {
+                       this.state.display == "Profile" && talent.id == this.state.id
+                       ? <Icon name='video' id={talent.id}  size='large' onClick={this.onClickVideo} />
+                       : <Icon name='user' id={talent.id}  size='large' onClick={this.onClickProfile} />
+                        }
+                        </Grid.Column>
+                        <Grid.Column>
+                        <Icon name='file pdf outline' value="ResumeDetail" size='large' />
+                        </Grid.Column>
+                        <Grid.Column>
+                        <Icon name='linkedin' value="linkedinDetail" size='large' />
+                        </Grid.Column>
+                        <Grid.Column>
+                        <Icon name='github' value="githubDetail" size='large' />
+                        </Grid.Column>
+                    </Grid.Row>
+                    </Grid>
+                    </Header>
+                </Card.Content>
+                <Card.Content extra>
+                <Button basic color='blue' content='C#' />
+                </Card.Content>
+            </Card> 
+                ))}
+           </div>
+        )
+    }
+}
+
---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/TalentFeed/TalentFeed.jsx
@@ -0,0 +1,158 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Cookies from 'js-cookie'
+import TalentCard from '../TalentFeed/TalentCard.jsx';
+import { Loader,Header, Grid, Container } from 'semantic-ui-react';
+import CompanyProfile from '../TalentFeed/CompanyProfile.jsx';
+import FollowingSuggestion from '../TalentFeed/FollowingSuggestion.jsx';
+import { BodyWrapper, loaderData } from '../Layout/BodyWrapper.jsx';
+
+export default class TalentFeed extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let loader = loaderData
+        loader.allowedUsers.push("Employer")
+        loader.allowedUsers.push("Recruiter")
+
+        this.state = {
+            loadNumber: 5,
+            loadPosition: 0,
+            feedData: [],
+            watchlist: [],
+            loaderData: loader,
+            loadingFeedData: false,
+            companyDetails:
+            {
+                companyContact:{
+                    name:'',
+                    phone:'',
+                    email:'',
+                    firstName:'',
+                    lastName:'',
+                    location:[]
+                },
+                primaryContact:[]
+            },
+            talent:[]
+        }
+        this.init = this.init.bind(this);
+        this.loadEmployerData=this.loadEmployerData.bind(this)
+        this.loadTalentData=this.loadTalentData.bind(this)
+        this.handleScroll=this.handleScroll.bind(this)
+
+    };
+    init() {
+        let loaderData = this.state.loaderData;
+       // loaderData.allowedUsers.push("Talent");
+        loaderData.isLoading = false;
+        this.setState({ loaderData, })
+        this.loadEmployerData()
+
+    }
+    componentDidMount() {
+        this.init();
+        this.loadTalentData();
+        window.addEventListener('scroll', this.handleScroll);
+    };
+    loadEmployerData() {
+        var cookies = Cookies.get('talentAuthToken');
+        $.ajax({
+            url: 'https://advancetaskprofile.azurewebsites.net/profile/profile/getEmployerProfile',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            //async:true,
+            success: function (res) {
+                let employerData = null;
+                if (res.employer) {
+                    employerData = res.employer.companyContact
+                    console.log(employerData)
+                    this.setState(
+                        {
+                            companyDetails:employerData
+                        }
+                    )
+                }
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        })
+       // this.init();
+    } 
+    loadTalentData()
+    {
+        console.log(this.state.position)
+        this.setState({loadingFeedData:true})
+        var cookies = Cookies.get('talentAuthToken');
+        $.ajax({
+            url: 'http://advancetaskprofile.azurewebsites.net/profile/profile/getTalent',
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            contentType: "application/json",
+            dataType: "json",
+            async:false,
+            data:{
+                increment: this.state.loadNumber,
+                position: this.state.loadPosition
+            },
+            success: function (res) {
+                this.setState(
+                    {
+                        talent:[...this.state.talent,...res.data],
+                        loadingFeedData:false
+                    }
+                )
+              console.log(res.data)
+            }.bind(this),
+            error: function (res) {
+                console.log(res.status)
+            }
+        })
+        this.init()
+    }
+    handleScroll(event) {
+        console.log('HEllo')
+        const win = $(window);
+        if ((($(document).height() - win.height()) == Math.round(win.scrollTop())) || ($(document).height() - win.height()) - Math.round(win.scrollTop()) == 1) {
+            $("#load-more-loading").show();
+            this.setState(
+                {
+                    loadPosition:this.state.loadPosition+5
+                }
+            )
+           this.loadTalentData();
+           console.log("bottom" + this.state.position)
+        }
+    }
+    render() {
+        return (
+            <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>        
+                 <div className="ui grid talent-feed container">
+                    <div className="four wide column">
+                        <CompanyProfile companyDetails={this.state.companyDetails}/>
+                    </div>
+                    <div className="eight wide column">
+                        <TalentCard talentDetail={this.state.talent}/>
+                    </div>
+                    <div className="four wide column">
+                        <div className="ui card">
+                            <FollowingSuggestion />
+                        </div>
+                    </div>
+                    
+                </div>
+                
+            </BodyWrapper>
+        )
+       
+    }
+}
